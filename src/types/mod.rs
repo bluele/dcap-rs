ccc1fd7a88503bf9b1a15d9f9fdf30244ee91ee9
Jun Kimura <junkxdev@gmail.com>
2024-12-27 11:23:43 +0900

    add `DCAPVerifierCommit`
    Signed-off-by: Jun Kimura <junkxdev@gmail.com>

--- src/types/mod.rs
+++ src/types/mod.rs
@@ -1,5 +1,5 @@
 use self::quotes::body::*;
-use crate::constants::{ENCLAVE_REPORT_LEN, SGX_TEE_TYPE, TD10_REPORT_LEN, TDX_TEE_TYPE};
+use crate::{constants::{ENCLAVE_REPORT_LEN, SGX_TEE_TYPE, TD10_REPORT_LEN, TDX_TEE_TYPE}, utils::hash::keccak256sum};
 use alloy_sol_types::SolValue;
 use serde::{Deserialize, Serialize};
 
@@ -150,5 +150,48 @@ impl VerifiedOutput {
             quote_body,
             advisory_ids,
         }
+    }
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct DCAPVerifierCommit {
+    pub attestation_time: u64,
+    pub sgx_intel_root_ca_hash: [u8; 32],
+    pub output: VerifiedOutput,
+}
+
+impl DCAPVerifierCommit {
+    pub fn new(attestation_time: u64, output: VerifiedOutput, sgx_intel_root_ca: &[u8]) -> Self {
+        Self {
+            attestation_time,
+            sgx_intel_root_ca_hash: keccak256sum(sgx_intel_root_ca),
+            output,
+        }
+    }
+
+    pub fn to_bytes(&self) -> Vec<u8> {
+        let mut output = self.output.to_bytes();
+        let mut bytes = Vec::new();
+        bytes.extend_from_slice(&self.attestation_time.to_le_bytes());
+        bytes.extend_from_slice(&self.sgx_intel_root_ca_hash);
+        bytes.append(&mut output);
+        bytes
+    }
+
+    pub fn from_bytes(slice: &[u8]) -> Self {
+        let mut attestation_time = [0; 8];
+        attestation_time.copy_from_slice(&slice[0..8]);
+        let mut sgx_intel_root_ca_hash = [0; 32];
+        sgx_intel_root_ca_hash.copy_from_slice(&slice[8..40]);
+        let output = VerifiedOutput::from_bytes(&slice[40..]);
+        Self {
+            attestation_time: u64::from_le_bytes(attestation_time),
+            sgx_intel_root_ca_hash,
+            output,
+        }
+    }
+
+    pub fn hash(&self) -> [u8; 32] {
+        keccak256sum(&self.to_bytes())
     }
 }
